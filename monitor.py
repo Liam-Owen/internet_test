b4a664bcca87ea3fe06ba13cdb57efac87de02e7
Liam Owen <72268903+Liam-Owen@users.noreply.github.com>
2021-09-09 21:58:42 +0100

    Project Start

---
+++ monitor.py
@@ -0,0 +1,21 @@
+#Speedtest package to monitor internet speed
+import speedtest
+
+#Packages to use date and time to identify when test was ran
+import datetime
+import time
+
+#Pandas library to clean and explore data returned
+import pandas as pd
+
+#Matplotlib package for data visualisation
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+
+
