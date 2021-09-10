fdb38f6a7a686c519ca198c8be26aca304538ba0
Liam Owen <72268903+Liam-Owen@users.noreply.github.com>
2021-09-10 19:58:08 +0100

    Complete  v1

--- monitor.py
+++ monitor.py
@@ -23,14 +23,14 @@ with open('internet_speed_test.csv', mode='w') as speedcsv:
     csv_writer.writeheader()
     
     #Set time for loop to end
-    endTime = datetime.datetime.now() + datetime.timedelta(minutes=10)
+    endTime = datetime.datetime.now() + datetime.timedelta(minutes=120)
     #Get internet speed with current time and convert to Mb/s
     while True:
         #If statement to check whether or not set time for the test to run has elapsed
         if datetime.datetime.now() >= endTime:
             break
         else:
-            current_time= datetime.datetime.now().strftime("%H:%M")
+            current_time= datetime.datetime.now().strftime('%d/%m/%Y, %H:%M')
             download= round((round(speed.download())/1048576),2)
             upload= round((round(speed.upload())/1048576),2)
             print(f"Time: {current_time}, Download Speed: {download} Mb/s, Upload Speed: {upload} Mb/s.")
@@ -43,7 +43,7 @@ with open('internet_speed_test.csv', mode='w') as speedcsv:
     })
     
     #10 Minute gap between checks
-            time.sleep(60)
+            time.sleep(300)
 
 
 
