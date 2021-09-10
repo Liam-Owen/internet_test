f59f13d1b7f9c61235ceb7ccf472e0a725319053
Liam Owen <72268903+Liam-Owen@users.noreply.github.com>
2021-09-10 18:16:00 +0100

    Initial speed test successful

--- monitor.py
+++ monitor.py
@@ -11,6 +11,16 @@ import pandas as pd
 #Matplotlib package for data visualisation
 import matplotlib.pyplot as plt
 
+#initialize speedtest
+speed=speedtest.Speedtest()
+
+#Get internet speed with current time and convert to Mb/s
+while True:
+    current_time= datetime.datetime.now().strftime("%H:%M")
+    download= round((round(speed.download())/1048576),2)
+    upload= round((round(speed.upload())/1048576),2)
+    print(f"Time: {current_time}, Download Speed: {download} Mb/s, Upload Speed: {upload} Mb/s.")
+
 
 
 
