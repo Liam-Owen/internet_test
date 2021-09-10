deebd5719f8600ec5eab6f98301022737518491a
Liam Owen <72268903+Liam-Owen@users.noreply.github.com>
2021-09-10 19:07:58 +0100

    Completed test of Code - Runs for 10 minutes checking speed every 1 minute and uploads output to csv

--- monitor.py
+++ monitor.py
@@ -11,16 +11,39 @@ import pandas as pd
 #Matplotlib package for data visualisation
 import matplotlib.pyplot as plt
 
+#Import CSV Package to save results
+import csv
+
 #initialize speedtest
 speed=speedtest.Speedtest()
 
-#Get internet speed with current time and convert to Mb/s
-while True:
-    current_time= datetime.datetime.now().strftime("%H:%M")
-    download= round((round(speed.download())/1048576),2)
-    upload= round((round(speed.upload())/1048576),2)
-    print(f"Time: {current_time}, Download Speed: {download} Mb/s, Upload Speed: {upload} Mb/s.")
-
+#Save results into a csv called internet_speed_test
+with open('internet_speed_test.csv', mode='w') as speedcsv:
+    csv_writer = csv.DictWriter(speedcsv, fieldnames=['time','download speed', 'upload speed'])
+    csv_writer.writeheader()
+    
+    #Set time for loop to end
+    endTime = datetime.datetime.now() + datetime.timedelta(minutes=10)
+    #Get internet speed with current time and convert to Mb/s
+    while True:
+        #If statement to check whether or not set time for the test to run has elapsed
+        if datetime.datetime.now() >= endTime:
+            break
+        else:
+            current_time= datetime.datetime.now().strftime("%H:%M")
+            download= round((round(speed.download())/1048576),2)
+            upload= round((round(speed.upload())/1048576),2)
+            print(f"Time: {current_time}, Download Speed: {download} Mb/s, Upload Speed: {upload} Mb/s.")
+    
+    #Write output to csv
+            csv_writer.writerow({
+                'time':current_time,
+                'download speed':download,
+                'upload speed':upload
+    })
+    
+    #10 Minute gap between checks
+            time.sleep(60)
 
 
 
