43ba3e4bd2d122cc4c95ea80a82532172cba8e9d
Liam Owen <72268903+Liam-Owen@users.noreply.github.com>
2021-09-14 17:34:27 +0100

    Split date and time into two columns, changed total run time to 1 hour with check every 1 minute

--- monitor.py
+++ monitor.py
@@ -1,49 +1,51 @@
-#Speedtest package to monitor internet speed
+# Speedtest package to monitor internet speed
 import speedtest
 
-#Packages to use date and time to identify when test was ran
+# Packages to use date and time to identify when test was ran
 import datetime
 import time
 
-#Import CSV Package to save results
+# Import CSV Package to save results
 import csv
 
-#Import pydrive to connect to GoogleDrive API and upload output of test to Google Drive
+# Import pydrive to connect to GoogleDrive API and upload output of test to Google Drive
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
-#initialize speedtest
+# initialize speedtest
 speed=speedtest.Speedtest()
 
-#Save results into a csv called internet_speed_test
+# Save results into a csv called internet_speed_test
 with open('internet_speed_test.csv', mode='w') as speedcsv:
-    csv_writer = csv.DictWriter(speedcsv, fieldnames=['time','download speed', 'upload speed'])
+    csv_writer = csv.DictWriter(speedcsv, fieldnames=['date','time','download speed', 'upload speed'])
     csv_writer.writeheader()
     
-    #Set time for loop to end
-    endTime = datetime.datetime.now() + datetime.timedelta(minutes=3)
-    #Get internet speed with current time and convert to Mb/s
+    # Set time for loop to end
+    endTime = datetime.datetime.now() + datetime.timedelta(minutes=60)
+    # Get internet speed with current time and convert to Mb/s
     while True:
-        #If statement to check whether or not set time for the test to run has elapsed
+        # If statement to check whether or not set time for the test to run has elapsed
         if datetime.datetime.now() >= endTime:
             break
         else:
-            current_time= datetime.datetime.now().strftime('%d/%m/%Y, %H:%M')
+            current_date = datetime.datetime.now('%d/%m/%Y')
+            current_time = datetime.datetime.now().strftime('%H:%M')
             download= round((round(speed.download())/1048576),2)
             upload= round((round(speed.upload())/1048576),2)
             print(f"Time: {current_time}, Download Speed: {download} Mb/s, Upload Speed: {upload} Mb/s.")
     
-    #Write output to csv
+    # Write output to csv
             csv_writer.writerow({
-                'time':current_time,
-                'download speed':download,
-                'upload speed':upload
+                'date': current_date,
+                'time': current_time,
+                'download speed': download,
+                'upload speed': upload
     })
     
-    #5 Minute gap between checks
+    # 1 Minute gap between checks
             time.sleep(60)
 
-#Upload internet_speed_test CSV to google drive
+# Upload internet_speed_test CSV to google drive
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
 
